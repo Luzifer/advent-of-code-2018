cee71706f17cc46d2463a4d05757e455c730e92c
Knut Ahlers <knut@ahlers.me>
2018-12-01 14:44:26 +0100

    Add day 01/2018

---
+++ 01.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def loop_until_duplicate(numbers):
+    encounters = [0]
+    state = 0
+    loops = 0
+    while True:
+        for n in numbers:
+            state += n
+
+            if state in encounters:
+                return state
+
+            encounters.append(state)
+
+        loops += 1
+        if loops % 1000 == 0:
+            print('Loops: {}'.format(loops))
+
+
+def main():
+    numbers = []
+    with open('01.txt') as f:
+        for line in f:
+            numbers.append(int(line))
+
+    print('Solution part 1: {}'.format(sum(numbers)))
+
+    print('Solution part 2: {}'.format(loop_until_duplicate(numbers)))
+
+
+if __name__ == '__main__':
+    main()
