76f75648729b581866d7f379bdb4a44c60fe8622
Knut Ahlers <knut@ahlers.me>
2018-12-01 14:49:15 +0100

    Rename for better sorting in GIST
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

--- day01.py
+++ day01.py
@@ -22,7 +22,7 @@ def loop_until_duplicate(numbers):
 
 def main():
     numbers = []
-    with open('01.txt') as f:
+    with open('day01.txt') as f:
         for line in f:
             numbers.append(int(line))
 
