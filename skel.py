b60a789964774f2555fa4320addd0541fe08d6fd
Knut Ahlers <knut@ahlers.me>
2018-12-07 14:21:38 +0100

    Add a skeleton for new days
    Signed-off-by: Knut Ahlers <knut@ahlers.me>

---
+++ skel.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def solve_problem1(indata):
+    pass
+
+
+def solve_problem2(indata):
+    pass
+
+
+def main():
+    indata = ""
+    with open('data/day05.txt') as f:
+        indata = f.read().strip()
+
+    print('Solution part 1: {}'.format(solve_problem1(indata)))
+    print('Solution part 2: {}'.format(solve_problem2(indata)))
+
+
+if __name__ == '__main__':
+    main()
